9da4bd057d5d80e1048d996879f7c34e5d974b25
shhyaam29 <singushyam85@gmail.com>
2025-06-24 14:05:15 +0530

    Update INDEX.C

--- INDEX.C
+++ INDEX.C
@@ -2,9 +2,15 @@
 
 int main() {
     int n;
+    printf("Enter a number (0–9): ");
     scanf("%d", &n);
-    if(n >= 0 && n <= 4) printf("Class A\n");
-    else if(n >= 5 && n <= 9) printf("Class B\n");
-    else printf("Invalid\n");
+
+    if(n >= 0 && n <= 4)
+        printf("Class A\n");
+    else if(n >= 5 && n <= 9)
+        printf("Class B\n");
+    else
+        printf("Invalid\n");
+
     return 0;
 }
